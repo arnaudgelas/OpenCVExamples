34b1fb44b4150246627163f720f8d7bfa37de6ab
Arnaud Gelas <arnaudgelas@gmail.com>
2012-04-03 13:06:26 +0200

    Error message if no arg

--- GetImageSize/GetImageSize.cpp
+++ GetImageSize/GetImageSize.cpp
@@ -6,6 +6,12 @@
 
 int main( int argc, char* argv[] )
 {
+  if( argc != 2 )
+    {
+    std::cerr << "Usage: " << argv[0] << "<InputImage>" << std::endl;
+    return EXIT_FAILURE;
+    }
+
   IplImage* img = cvLoadImage( argv[1] );
   if( !img )
     {
