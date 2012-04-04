a170c25fd0a76ea6a2cdf9bf976fc0e2ad8c2306
Arnaud Gelas <arnaudgelas@gmail.com>
2012-04-04 10:04:40 +0200

    Display an image

---
+++ DisplayAnImage/DisplayAnImage.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#include "cv.h"
+#include "highgui.h"
+
+int main( int argc, char* argv[] )
+{
+  if( argc != 2 )
+    {
+    std::cerr << "Usage: " << argv[0] << " <InputImage>" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  IplImage* image = cvLoadImage( argv[1] );
+  if( !image )
+    {
+    std::cerr << "Could not load image file: " << argv[1] << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  cv::namedWindow( argv[1], CV_WINDOW_AUTOSIZE );
+  cv::imshow( argv[1], image );
+
+  cvReleaseImage( &image );
+  cv::waitKey(0);
+
+  return EXIT_SUCCESS;
+}
