368e979f50c4c4c936c7e857678127b18b6e8047
Arnaud Gelas <arnaudgelas@gmail.com>
2012-04-12 19:23:50 +0200

    only CV_HOUGH_GRADIENT is available for HoughCircles

--- HoughCircles/HoughCircles.cpp
+++ HoughCircles/HoughCircles.cpp
@@ -19,21 +19,49 @@ int main( int argc, char* argv[] )
     }
 
   std::vector< cv::Vec3f > circles;
-  int method = CV_HOUGH_MULTI_SCALE;
-  double dp = 10.;
-  double minDist = 5.;
-  double param1=100.;
-  double param2=100.;
-  int minRadius = 0;
-  int maxRadius = 0;
+  int method = CV_HOUGH_GRADIENT;
+  /*
+  double dp = 2.;
+  double minDist = 10.;
+  double param1=30.;
+  double param2=15.;
+  int minRadius = 5;
+  int maxRadius = 50;
+*/
 
+  // The inverse ratio of the accumulator resolution to the image resolution.
+  // For example, if dp=1 , the accumulator will have the same resolution as the
+  // input image, if dp=2 - accumulator will have half as big width and height, etc
+  double dp = 1.;
+
+  // Minimum distance between the centers of the detected circles. If the parameter
+  // is too small, multiple neighbor circles may be falsely detected in addition
+  // to a true one. If it is too large, some circles may be missed
+  double minDist = 5.;
+
+  // The first method-specific parameter. in the case of CV_HOUGH_GRADIENT it is
+  // the higher threshold of the two passed to Canny() edge detector (the lower
+  // one will be twice smaller)
+  double param1=100.;
+
+  // The second method-specific parameter. in the case of CV_HOUGH_GRADIENT it is
+  // the accumulator threshold at the center detection stage. The smaller it is,
+  // the more false circles may be detected. Circles, corresponding to the larger
+  // accumulator values, will be returned first
+  double param2=20.;
+
+  // Minimum circle radius
+  int minRadius = 5;
+
+  // Maximum circle radius
+  int maxRadius = 100;
   cv::HoughCircles( image, circles, method, dp, minDist, param1, param2,
                 minRadius, maxRadius );
 
   CvScalar color;
   color.val[0] = 255.;
 
-  int thickness = -1;
+  int thickness = 3;
 
   size_t i = 1;
 
@@ -43,10 +71,10 @@ int main( int argc, char* argv[] )
     {
       std::cout << "feature point " << i << std::endl;
       std::cout << "position: [ " << ( *it )[0] << ", " << ( *it )[1] << "]" << std::endl;
-
+      std::cout << "radius: " << ( *it )[2] << std::endl;
       std::cout << std::endl;
 
-      cvCircle( image, cv::Point( ( *it )[0], ( *it )[1] ), 10, color, thickness );
+      cvCircle( image, cv::Point( ( *it )[0], ( *it )[1] ), (*it)[2], color, thickness );
     }
 
   cv::namedWindow( argv[1], CV_WINDOW_NORMAL );
